0c36da5afc40a83d24541c1ef3f27b1f599bcf1f
chrismol2004 <61835441+chrismol2004@users.noreply.github.com>
2020-03-09 18:46:05 -0300

    Criado os filhos - membro complexo da classe Pessoa

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,15 +1,18 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=45):
+    def __init__(self, *filhos, nome=None, idade=45 ):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f"Olá!!! {id(self)}"
 
 
 if __name__ == "__main__":
-    p = Pessoa('Christiane')
+    f = Pessoa(nome='Christiane')
+    p = Pessoa(f, nome='Theresinha')
     print(Pessoa.cumprimentar(p))  # chamada do método
     print(p.cumprimentar())  # chamada do método
     print(p.nome)  # acesso ao parâmetro
-    p.nome = "Christiane"  # atribuindo valor ao parâmetro
+    for filho in p.filhos:
+    print(filho.nome)
