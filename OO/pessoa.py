d7bfbb99248d6794497b9d4e457c5b7dc7d9f389
chrismol2004 <61835441+chrismol2004@users.noreply.github.com>
2020-03-09 18:33:38 -0300

    Criado os membros da classe pessoa

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,9 +1,15 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=45):
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self):
         return f"Olá!!! {id(self)}"
 
-if __name__ == "__name__":
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
+
+if __name__ == "__main__":
+    p = Pessoa('Christiane')
+    print(Pessoa.cumprimentar(p))  # chamada do método
+    print(p.cumprimentar())  # chamada do método
+    print(p.nome)  # acesso ao parâmetro
+    p.nome = "Christiane"  # atribuindo valor ao parâmetro
