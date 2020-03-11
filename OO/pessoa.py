24d5c849b23ff3f6753e7390f1126c1d7f5a0d94
chrismol2004 <61835441+chrismol2004@users.noreply.github.com>
2020-03-11 15:17:24 -0300

    Criação do metodo de classe

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,13 +1,21 @@
 class Pessoa:
     olhos = 2
 
-    def __init__(self, *filhos, nome=None, idade=45 ):
+    def __init__(self, *filhos, nome=".", idade=45):
         self.nome = nome
         self.idade = idade
         self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f"Olá!!! {id(self)}"
+
+    @staticmethod
+    def metodo_estatico():
+        return 198
+
+    @classmethod
+    def mostra_olhos(cls):
+        return f' A {cls.nome} tem {cls.olhos} olhos!!! '
 
 
 if __name__ == "__main__":
@@ -24,7 +32,10 @@ if __name__ == "__main__":
     print(p.__dict__)
     print(f.__dict__)
 
-    del p.sobrenome   # deleta o atributo
-    p.olhos = 1
+    del p.sobrenome  # deleta o atributo
+    p.olhos = 10
     print(Pessoa.olhos)
     print(p.olhos)
+    print(Pessoa.metodo_estatico(), f.metodo_estatico())
+    print(f.mostra_olhos(), p.mostra_olhos())
+
